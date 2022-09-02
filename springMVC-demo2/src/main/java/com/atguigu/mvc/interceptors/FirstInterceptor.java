d48da9e4835c1e13a249f2450c8638b9afd68b9b
Haito <1940307360@qq.com>
2022-09-02 16:56:53 +0800

    second commit

--- springMVC-demo2/src/main/java/com/atguigu/mvc/interceptors/FirstInterceptor.java
+++ springMVC-demo2/src/main/java/com/atguigu/mvc/interceptors/FirstInterceptor.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 public class FirstInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("preHandle");
+        System.out.println("preHandle 1");
         return true;  //表示放行
     }
 
